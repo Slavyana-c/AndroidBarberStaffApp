6143818dfe0af1f10b357354fda649a9bbb67edb
slavyana <slavyana.c@gmail.com>
2020-04-05 21:59:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androidbarberstaffapp/MyStateAdapter.java
@@ -0,0 +1,79 @@
+package com.example.androidbarberstaffapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidbarberstaffapp.Interface.IRecyclerItemSelectedListener;
+import com.example.androidbarberstaffapp.Model.City;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MyStateAdapter extends RecyclerView.Adapter<MyStateAdapter.MyViewHolder> {
+
+    Context context;
+    List<City> cityList;
+
+    public MyStateAdapter(Context context, List<City> cityList) {
+        this.context = context;
+        this.cityList = cityList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.layout_state, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.txt_state_name.setText(cityList.get(position).getName());
+
+        holder.setiRecyclerItemSelectedListener(new IRecyclerItemSelectedListener() {
+            @Override
+            public void onItemSelectedListener(View view, int pos) {
+                // Implement later
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cityList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.txt_state_name)
+        TextView txt_state_name;
+
+        IRecyclerItemSelectedListener iRecyclerItemSelectedListener;
+
+        public void setiRecyclerItemSelectedListener(IRecyclerItemSelectedListener iRecyclerItemSelectedListener) {
+            this.iRecyclerItemSelectedListener = iRecyclerItemSelectedListener;
+        }
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            iRecyclerItemSelectedListener.onItemSelectedListener(v, getAdapterPosition());
+        }
+    }
+}
