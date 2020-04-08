690767fc07fc6da17efcfd07cd822a9e22d3c152
slavyana <slavyana.c@gmail.com>
2020-04-08 14:19:34 +0100

    Work on adding real time notifications.

--- app/src/main/java/com/example/androidbarberstaffapp/StaffHomeActivity.java
+++ app/src/main/java/com/example/androidbarberstaffapp/StaffHomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidbarberstaffapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +12,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.androidbarberstaffapp.Adapter.MyTimeSlotAdapter;
@@ -26,7 +30,10 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -54,6 +61,12 @@ public class StaffHomeActivity extends AppCompatActivity implements ITimeSlotLoa
     RecyclerView recycler_time_slot;
     @BindView(R.id.calendarView)
     HorizontalCalendarView calendarView;
+
+    TextView txt_notification_badge;
+    CollectionReference notificationCollection;
+
+    EventListener<QuerySnapshot> notificationEvent;
+    ListenerRegistration notificationListener;
 
     ActionBarDrawerToggle actionBarDrawerToggle;
 
@@ -235,6 +248,28 @@ public class StaffHomeActivity extends AppCompatActivity implements ITimeSlotLoa
 
     private void init() {
         iTimeSlotLoadListener = this;
+        initNotificatiomRealTimeUpdate();
+    }
+
+    private void initNotificatiomRealTimeUpdate() {
+        notificationCollection = FirebaseFirestore.getInstance()
+                .collection("AllSalons")
+                .document(Common.state_name)
+                .collection("Branch")
+                .document(Common.selectedSalon.getSalonId())
+                .collection("Barber")
+                .document(Common.currentBarber.getBarberId())
+                .collection("Notifications");
+
+        notificationEvent = new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(queryDocumentSnapshots.size() > 0) {
+                    loadNotification();
+                }
+            }
+        };
+        notificationListener = notificationCollection.whereEqualTo("read", false).addSnapshotListener(notificationEvent); // Unread notifications
     }
 
     @Override
@@ -280,5 +315,36 @@ public class StaffHomeActivity extends AppCompatActivity implements ITimeSlotLoa
         recycler_time_slot.setAdapter(adapter);
 
         alertDialog.dismiss();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.staff_home_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_new_notification);
+
+        txt_notification_badge = (TextView)menuItem.getActionView()
+                .findViewById(R.id.notification_badge);
+        
+        loadNotification();
+
+
+        menuItem.getActionView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOptionsItemSelected(menuItem);
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void loadNotification() {
+        notificationCollection.whereEqualTo("read", false)
+                .get()
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // ADD ON FAILURE
+                    }
+                });
     }
 }
