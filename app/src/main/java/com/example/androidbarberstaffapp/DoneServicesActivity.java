d4e55cc9a835de4b64156a4c4e7cf91708489f0d
slavyana <slavyana.c@gmail.com>
2020-04-09 20:40:18 +0100

    Added done services layout.

---
+++ app/src/main/java/com/example/androidbarberstaffapp/DoneServicesActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidbarberstaffapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoneServicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done_services);
+    }
+}
